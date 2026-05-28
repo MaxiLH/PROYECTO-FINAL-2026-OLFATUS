d8a92ea46307512e4888b96dc3d0fa517c918aa7
MaxiLH <48799507@est.ort.edu.ar>
2026-05-28 08:25:12 -0300

    Códigos de prueba acttualizados

--- MQS_GENERAL_ANALOGICO_PRUEBA/MQS_GENERAL_ANALOGICO_PRUEBA.ino
+++ MQS_GENERAL_ANALOGICO_PRUEBA/MQS_GENERAL_ANALOGICO_PRUEBA.ino
@@ -6,9 +6,16 @@ void loop() {
   
   int adc_MQ = analogRead(A0); //Lemos la salida analógica del MQ
   float voltaje = adc_MQ * (5.0 / 1023.0); //Convertimos la lectura en un valor de voltaje
+  int adc_MQ1 = analogRead(A1); //Lemos la salida analógica del MQ
+  float voltaje2 = adc_MQ1 * (5.0 / 1023.0); //Convertimos la lectura en un valor de voltaje
 
   Serial.print("adc:");
   Serial.print(adc_MQ);
   Serial.print("    voltaje:");
-  Serial.println(voltaje);
+  Serial.print(voltaje);
+  Serial.print("     ");
+  Serial.print("adc:");
+  Serial.print(adc_MQ1);
+  Serial.print("    voltaje:");
+  Serial.println(voltaje2);
 }
